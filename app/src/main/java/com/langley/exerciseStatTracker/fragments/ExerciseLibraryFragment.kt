d748cdec6dbff49957424b85380fed3245e65acf
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2023-08-10 19:57:17 -0700

    Continued work on exercise list fragment.

--- app/src/main/java/com/langley/exerciseStatTracker/fragments/ExerciseLibraryFragment.kt
+++ app/src/main/java/com/langley/exerciseStatTracker/fragments/ExerciseLibraryFragment.kt
@@ -5,16 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.langley.exerciseStatTracker.R
+import com.langley.exerciseStatTracker.adapters.ExerciseLibraryAdapter
+import com.langley.exerciseStatTracker.data.ExerciseAppViewModel
 
 class ExerciseLibraryFragment : Fragment() {
+
+    private lateinit var viewModel: ExerciseAppViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise_library, container, false)
+        val view = inflater.inflate(R.layout.fragment_exercise_library, container, false)
+
+        //Setup recycler view and adapter
+        val adapter = ExerciseLibraryAdapter()
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        //Setup view model
+        val viewModel = ViewModelProvider(this)[ExerciseAppViewModel::class.java]
+
+
+
+
+
+
+        return view
     }
 
 }
